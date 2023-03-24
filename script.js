a3bd9876626135f7ef5e9561e538184d485fe1c0
Sebastian Sala <sebastian_orko@hotmail.com>
2023-03-24 17:12:48 -0600

    ia medio funcional

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-const gameBoard = (() => {})();
+function getRandomInt(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
 
 const displayController = (() => {
   let playerXMoves = [];
   let playerYMoves = [];
+  const moves = [0, 1, 2, 3, 4, 5, 6, 7, 8];
   const item = document.querySelectorAll(".item");
   const currentPlayer = document.getElementById("player");
 
@@ -12,7 +15,8 @@ const displayController = (() => {
     });
     playerXMoves = [];
     playerYMoves = [];
-    currentPlayer.textContent = 'X';
+    currentPlayer.textContent = "X";
+    render();
   };
 
   const checkWinner = (selections) => {
@@ -23,6 +27,59 @@ const displayController = (() => {
     });
   };
 
+  const updateBoardWithIA = (element) => {
+    let filteredMoves = moves.filter((move) => {
+      return !playerXMoves.includes(move) && !playerYMoves.includes(move);
+    });
+    if (element.textContent !== "") {
+      return;
+    }
+    if (currentPlayer.textContent === "X") {
+      element.innerHTML = currentPlayer.textContent;
+      playerXMoves.push(parseInt(element.id));
+      if (checkWinner(playerXMoves)) {
+        alert("X wins!");
+        clear();
+      }
+    } else {
+      const randomIndex = getRandomInt(filteredMoves);
+      const move = filteredMoves[randomIndex];
+      playerYMoves.push(move);
+      const moveElement = document.getElementById(`${move}`);
+      moveElement.innerHTML = currentPlayer.textContent;
+      if (checkWinner(playerYMoves)) {
+        alert("O wins!");
+        clear();
+      }
+    }
+    currentPlayer.textContent = currentPlayer.textContent === "X" ? "O" : "X";
+  };
+
+  const render = () => {
+    item.forEach((element) => {
+      element.addEventListener("click", () => {
+        updateBoardWithIA(element);
+      });
+    });
+  };
+
+  const winConditions = [
+    [0, 1, 2],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 5, 8],
+  ];
+
+  return { render };
+})();
+
+displayController.render();
+
+/*
   const updateBoard = (element) => {
     if (element.textContent !== "") {
       return;
@@ -44,28 +101,4 @@ const displayController = (() => {
       }
     }
     currentPlayer.textContent = currentPlayer.textContent === "X" ? "O" : "X";
-  };
-
-  const render = () => {
-    item.forEach((element) => {
-      element.addEventListener("click", () => {
-        updateBoard(element);
-      });
-    });
-  };
-
-  const winConditions = [
-    [0, 1, 2],
-    [0, 4, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8],
-    [2, 5, 8],
-  ];
-
-  return {render};
-})();
-
-displayController.render();
+  };*/
